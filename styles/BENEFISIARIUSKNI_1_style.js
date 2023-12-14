7d7fa3e90ca94a967183c3a6c33ee4f1af42b7fa
DNPE23immanuel <153790934+DNPE23immanuel@users.noreply.github.com>
2023-12-14 14:58:49 +0900

    Add files via upload

---
+++ styles/BENEFISIARIUSKNI_1_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_BENEFISIARIUSKNI_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "15.600000000000001px \'Arial\', sans-serif";
+    var labelFill = "#fdfcfc";
+    var bufferColor = "#fa082c";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Title") !== null) {
+        labelText = String(feature.get("Title"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(16,32,253,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
